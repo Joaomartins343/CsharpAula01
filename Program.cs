a9dfef704443d90f63287616d58803db7efe46e1
Joao Vítor Martins <0001271556@senaimgaluno.com.br>
2026-06-18 17:00:22 -0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -129,4 +129,227 @@ else{
     Console.WriteLine("---------------------------------------");
 
 //Ativ 11 - Sistema de faccoes
+ Console.Write("Digite o seu ID de registro: ");
+        int id = Convert.ToInt32(Console.ReadLine());
 
+        if (id % 2 == 0)
+        {
+            Console.WriteLine("Facção designada: Magos do Alvorecer");
+        }
+        else
+        {
+            Console.WriteLine("Facção designada: Guerreiros da Noite");
+        }
+    }
+
+    static void ExecutarEx12()
+    {
+        Console.WriteLine("--- 12. Validação de Cheat Code ---");
+        Console.Write("Username: ");
+        string user = Console.ReadLine();
+        Console.Write("Password: ");
+        string pass = Console.ReadLine();
+
+        if (user == "godmode" && pass == "666")
+        {
+            Console.WriteLine("Trapaça Ativada: Modo Imortal Habilitado");
+        }
+        else
+        {
+            Console.WriteLine("Código Inválido");
+        }
+    }
+
+     //ativ12 -  Peso do Inventário de Carga
+        Console.Write("Peso atual da mochila (kg): ");
+        double peso = Convert.ToDouble(Console.ReadLine());
+
+        if (peso > 50.0)
+        {
+            Console.WriteLine("Aviso: Sobrecarga de peso detectada! O personagem não consegue correr.");
+        }
+        else
+        {
+            Console.WriteLine("Peso dentro do limite. Você pode correr normalmente.");
+        }
+    }
+
+    //Ativ 13 - Ranking Competitivo por MMR
+        Console.Write("Digite sua pontuação de MMR: ");
+        int mmr = Convert.ToInt32(Console.ReadLine());
+
+        if (mmr < 1000)
+        {
+            Console.WriteLine("Classificação: Rank Bronze");
+        }
+        else if (mmr >= 1000 && mmr <= 2000)
+        {
+            Console.WriteLine("Classificação: Rank Prata");
+        }
+        else
+        {
+            Console.WriteLine("Classificação: Rank Ouro");
+        }
+    }
+
+//Ativ 15 - Spawner Sequencial de Inimigos
+        for (int i = 1; i <= 50; i++)
+        {
+            Console.WriteLine($"Inimigo #{i} gerado.");
+        }
+    }
+
+//Ativ 16 - Spawn do Boss
+        Console.WriteLine("O escudo do Boss será ativado nos seguintes rounds:");
+        for (int i = 2; i <= 100; i += 2)
+        {
+            Console.WriteLine($"Round {i}");
+        }
+    }
+
+//Ativ 17 - Contagem Regressiva do Round
+        for (int i = 10; i >= 0; i--)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine("FIGHT! COMEÇOU O ROUND!");
+    }
+
+//Ativ 18 - Farm Semanal Acumulado
+        int totalMoedas = 0;
+
+        for (int i = 1; i <= 7; i++)
+        {
+            Console.Write($"Moedas farmadas no Dia {i}: ");
+            totalMoedas += Convert.ToInt32(Console.ReadLine());
+        }
+        Console.WriteLine($"\nMontante total arrecadado na semana: {totalMoedas} moedas.");
+    }
+
+//Ativ 19 - Multiplicador de Combo de Dano 
+        Console.Write("Qual o dano base do ataque do herói? ");
+        double danoBase = Convert.ToDouble(Console.ReadLine());
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine($"{i}x Acertos | Dano Total: {danoBase * i}");
+        }
+    }
+
+//Ativ 20 - Quebrador de Baús
+        string senha = "";
+
+        while (senha != "ABRACADABRA")
+        {
+            Console.Write("Tente adivinhar a palavra-chave do baú lendário: ");
+            senha = Console.ReadLine();
+
+            if (senha != "ABRACADABRA")
+            {
+                Console.WriteLine("Incorreto! O baú permanece trancado.\n");
+            }
+        }
+        Console.WriteLine("Baú destrancado! Você encontrou itens lendários!");
+    }
+
+//Ativ 21 - Dungeon Customizável
+        Console.Write("Quantas salas (N) a dungeon terá? ");
+        int nSalas = Convert.ToInt32(Console.ReadLine());
+
+        double somaDificuldade = 0;
+
+        for (int i = 1; i <= nSalas; i++)
+        {
+            Console.Write($"Dificuldade da Sala {i} (1 a 100): ");
+            somaDificuldade += Convert.ToDouble(Console.ReadLine());
+        }
+
+        double media = somaDificuldade / nSalas;
+        Console.WriteLine($"\nA média de dificuldade geral do mapa é: {media}");
+    }
+
+//Ativ 22 - Simulador de Drop Rate (Loot Box) ---");
+        int lootComum = 0;
+        int lootRaro = 0;
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.Write($"Raridade da {i}ª caixa (0 a 100): ");
+            int raridade = Convert.ToInt32(Console.ReadLine());
+
+            if (raridade < 50)
+            {
+                lootComum++;
+            }
+            else
+            {
+                lootRaro++;
+            }
+        }
+
+        Console.WriteLine("\n--- Balanço do Inventário ---");
+        Console.WriteLine($"Loot Comum: {lootComum}");
+        Console.WriteLine($"Loot Raro: {lootRaro}");
+    }
+
+//Ativ 23 - Recorde Absoluto de Pontos (High Score) ---");
+        int maiorPontuacao = 0;
+        int pontuacaoAtual = -1;
+
+        Console.WriteLine("Digite as pontuações dos jogadores. (Digite 0 para encerrar)");
+
+        while (pontuacaoAtual != 0)
+        {
+            Console.Write("Pontuação: ");
+            pontuacaoAtual = Convert.ToInt32(Console.ReadLine());
+
+            if (pontuacaoAtual > maiorPontuacao)
+            {
+                maiorPontuacao = pontuacaoAtual;
+            }
+        }
+        Console.WriteLine($"\nA Captação foi encerrada. Maior pontuação registrada: {maiorPontuacao}");
+    }
+
+//Ativ 24 - Gasto e Esvaziamento de Mana ---");
+        int mana = 1000;
+
+        while (mana > 0)
+        {
+            Console.WriteLine($"Mana Atual: {mana}");
+            Console.Write("Quanto de mana o feitiço custou? ");
+            int gasto = Convert.ToInt32(Console.ReadLine());
+
+            mana -= gasto; 
+        }
+
+        Console.WriteLine("\nMana Esgotada! Recarregue.");
+    }
+
+//Ativ 25 - Painel MVP (Most Valuable Player) ---");
+        int totalAbatesCla = 0;
+        int maiorNumeroAbates = -1;
+        string nomeMVP = "";
+
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine($"\nJogador {i}:");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Número de Abates (Kills): ");
+            int abates = Convert.ToInt32(Console.ReadLine());
+
+            totalAbatesCla += abates;
+
+    
+            if (abates > maiorNumeroAbates)
+            {
+                maiorNumeroAbates = abates;
+                nomeMVP = nome;
+            }
+        }
+
+        Console.WriteLine("\n--- RELATÓRIO DO ROUND ---");
+        Console.WriteLine($"Somatório de Abates do Clã: {totalAbatesCla} Kills");
+        Console.WriteLine($"MVP da Rodada: {nomeMVP} com {maiorNumeroAbates} eliminações!");
